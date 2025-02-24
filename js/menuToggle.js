51f4d8a6753957f0bfcea2a6b80652ec83e64ae1
Oleh Konyk <drmom4ik@gmail.com>
2025-02-24 21:03:07 +0200

    nav-menu

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,6 +1,8 @@
+// Відкриття/закриття меню
 document.getElementById("nav-btn").addEventListener("click", function () {
     document.getElementById("nav-menu").classList.toggle("show");
 });
+
 
 document.addEventListener("click", function (event) {
     const menu = document.getElementById("nav-menu");
@@ -9,4 +11,21 @@ document.addEventListener("click", function (event) {
     if (!menu.contains(event.target) && !button.contains(event.target)) {
         menu.classList.remove("show");
     }
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector('header').offsetHeight, 
+            behavior: 'smooth'
+        });
+
+
+        document.getElementById("nav-menu").classList.remove("show");
+    });
 });
